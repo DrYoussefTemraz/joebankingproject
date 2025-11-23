5c9b6d328d0253b333149fdbee9200b86bb7001b
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-11-23 15:28:45 +0200

    Implementing Header Component and Props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
+    const loggedIn = {firstName : 'Joesam'}
     return (
         <section className="home">
             <div className="home-content">
                 <header className="home-header">
-                    Welcome, Youssef
+                 <HeaderBox
+                 type = 'greeting'
+                 title = 'Welcome'
+                 user={loggedIn?.firstName || "Guest" }
+                 subtext = 'Access and Manage your account and transactions Effeciently'
+                 />
+                 <TotalBalanceBox
+                 accounts = {[]}
+                 totalBanks = {1}
+                 totalCurrentBalance = {1250.35}
+
+
+                 
+                 />
                 </header>
             </div>
 
