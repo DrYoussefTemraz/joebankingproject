9529441be737f4a9f395b1066d2876d58384d3da
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-11-24 13:00:56 +0200

    styling a responsive sidebar icons and text

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -10,33 +10,42 @@ const SideBar = ({ user }: SiderbarProps) => {
     return (
         <section className='sidebar'>
             <nav className='flex flex-col gap-4'>
-                <Link href= '/' className='mb-12 cursor-pointer items-center gap-2'>
-                <Image
-                src= '/icons/logo.svg'
-                alt='horizon'
-                width={34}
-                height={34}
-                className='size-[24px] max-xl:size-14'
-                />
-                <h1 className='sidebar-logo'>Horizon</h1>
+                <Link href='/' className='flex mb-12 cursor-pointer items-center gap-2'>
+                    <Image
+                        src='/icons/logo.svg'
+                        alt='horizon'
+                        width={34}
+                        height={34}
+                        className='size-[24px] max-xl:size-14'
+                    />
+                    <h1 className='sidebar-logo'>Horizon</h1>
                 </Link>
                 {sidebarLinks.map((item) => {
                     const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
-                    return(
-                        <Link 
-                        href={item.route} 
-                        key={item.label}
-                        className={cn('sidebar-link', {'bg-bank-gradient' : isActive})}
-                            
-                        
+                    return (
+                        <Link
+                            href={item.route}
+                            key={item.label}
+                            // using the cn for classes only applied on specific condition
+                            className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
                         >
-                            {item.label}
+                            <div className='relative size-6'>
+                                <Image
+                                    src={item.imgURL}
+                                    alt={item.label}
+                                    fill
+                                    className={cn({ 'brightness-[3] invert-0': isActive })}
+                                />
+
+
+                            </div>
+                            <p className={cn('sidebar-label', { '!text-white': isActive })}>{item.label}</p>
                         </Link>
                     )
                 })}
-
+{/* USER */}
             </nav>
-            SideBar
+{/* FOOTER */}
         </section>
     )
 }
