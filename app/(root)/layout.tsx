d30f9601275f3ec12be496dc85d9bb33272450d5
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-11-24 23:53:11 +0200

    implementation of the left sidebar with sheetclose component and responsive

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,26 @@
+import MobileNav from "@/components/MobileNav";
 import SideBar from "@/components/SideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = {firstName : 'youssef', lastName :'Temraz'}
+  const logIn = { firstName: 'youssef', lastName: 'Temraz' }
   return (
-   <main className="flex h-screen w-full font-inter">
-    <SideBar user={loggedIn}/>
-    {children}
+    <main className=" flex h-screen w-full font-inter">
+      <SideBar user={logIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src='/icons/logo.svg' alt='logo' width={30} height={30} />
+          <div>
+            <MobileNav user={logIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+
     </main>
   );
 }
