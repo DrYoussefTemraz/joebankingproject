dfcae264d5d26de87924985d3344e64c79f0096b
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-11-30 00:11:54 +0200

    implementing the form for email

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,12 +22,11 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Divide } from 'lucide-react'
 
 
 const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
+    email: z.email(),
 })
 
 
@@ -41,7 +40,7 @@ const AuthForm = ({ type }: { type: string }) => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            username: "",
+            email: "",
         },
     })
 
@@ -102,18 +101,23 @@ const AuthForm = ({ type }: { type: string }) => {
                             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                                 <FormField
                                     control={form.control}
-                                    name="username"
+                                    name="email"
                                     render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Username</FormLabel>
-                                            <FormControl>
-                                                <Input placeholder="shadcn" {...field} />
-                                            </FormControl>
-                                            <FormDescription>
-                                                This is your public display name.
-                                            </FormDescription>
-                                            <FormMessage />
-                                        </FormItem>
+                                        <div className='form-item'>
+                                            <FormLabel className='form-label'>
+                                                Email
+                                            </FormLabel>
+                                            <div className='flex flex-col w-full'>
+                                                <FormControl>
+                                                    <Input
+                                                        placeholder="Enter your email"
+                                                        className='input-class'
+                                                        {...field}
+                                                    />
+                                                </FormControl>
+                                                <FormMessage className='form-message mt-2'/>
+                                            </div>
+                                        </div>
                                     )}
                                 />
                                 <Button type="submit">Submit</Button>
