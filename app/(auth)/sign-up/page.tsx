f31849517ae1fac102b63703c9c3a5eabbe86dd1
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-11-29 22:41:33 +0200

    initializing reusable auth form according to the route

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,15 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <section className='flex-center w-full  max-sm:px-6'>
+      <AuthForm 
+      type='sign-up'
+      />
+      </section>
   )
 }
 
 export default SignUp
+
